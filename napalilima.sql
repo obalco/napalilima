1f479bd32b358bd5242d0d4963e604d6eabd4eb6
obalco <obal.tom@gmail.com>
2012-09-25 21:59:49 +0200

    nova strukura...a zaciatok branchu v1.0

--- napalilima.sql
+++ napalilima.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 3.3.9
+-- version 3.4.5
 -- http://www.phpmyadmin.net
 --
 -- Počítač: localhost
--- Vygenerováno: Pátek 14. září 2012, 14:55
--- Verze MySQL: 5.1.53
--- Verze PHP: 5.3.4
+-- Vygenerováno: Úte 25. zář 2012, 16:27
+-- Verze MySQL: 5.5.16
+-- Verze PHP: 5.3.8
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -22,61 +23,78 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- --------------------------------------------------------
 
 --
+-- Struktura tabulky `claims`
+--
+
+CREATE TABLE IF NOT EXISTS `claims` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_u` int(11) NOT NULL,
+  `who` varchar(50) COLLATE utf8_czech_ci NOT NULL,
+  `how` text COLLATE utf8_czech_ci NOT NULL,
+  `date` varchar(16) COLLATE utf8_czech_ci NOT NULL,
+  `ip` varchar(16) COLLATE utf8_czech_ci NOT NULL,
+  `sys_date` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Struktura tabulky `comments`
 --
 
 CREATE TABLE IF NOT EXISTS `comments` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_staznosti` int(11) NOT NULL,
+  `id_u` int(11) NOT NULL,
+  `id_c` int(11) NOT NULL,
   `comment` text COLLATE utf8_czech_ci NOT NULL,
+  `sys_date` varchar(11) COLLATE utf8_czech_ci NOT NULL,
+  `ip` varchar(16) COLLATE utf8_czech_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci AUTO_INCREMENT=3 ;
-
---
--- Vypisuji data pro tabulku `comments`
---
-
-INSERT INTO `comments` (`id`, `id_staznosti`, `comment`) VALUES
-(1, 17, 'komentar'),
-(2, 18, 'komentar');
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabulky `staznosti`
+-- Struktura tabulky `likes`
 --
 
-CREATE TABLE IF NOT EXISTS `staznosti` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `nick` varchar(20) COLLATE utf8_czech_ci NOT NULL,
-  `email` varchar(40) COLLATE utf8_czech_ci NOT NULL,
-  `staznost_kedy` varchar(10) COLLATE utf8_czech_ci NOT NULL,
-  `staznost_na` varchar(40) COLLATE utf8_czech_ci NOT NULL,
-  `staznost` varchar(1024) COLLATE utf8_czech_ci NOT NULL,
-  `datum_staznost` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `ip` varchar(15) COLLATE utf8_czech_ci NOT NULL,
-  `browser` varchar(200) COLLATE utf8_czech_ci NOT NULL,
+CREATE TABLE IF NOT EXISTS `likes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci AUTO_INCREMENT=19 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
--- Vypisuji data pro tabulku `staznosti`
+-- Struktura tabulky `tokens`
 --
 
-INSERT INTO `staznosti` (`id`, `nick`, `email`, `staznost_kedy`, `staznost_na`, `staznost`, `datum_staznost`, `ip`, `browser`) VALUES
-(17, 'sdasd', 'asdasd', 'asdasd', 'sdad', 'asdasd', '2012-09-14 15:41:09', '127.0.0.1', 'browser'),
-(3, 'sadasd', 'sdasd', 'sdasda', 'dasdasdas', 'sdasdas', '2012-09-01 11:14:31', '127.0.0.1', 'browser'),
-(4, 'asdas', 'dasda', 'dasdas', 'asdasdad', 'asdasdas', '2012-09-01 11:14:36', '127.0.0.1', 'browser'),
-(5, 'asdas', 'dasda', 'dasdas', 'asdasdad', 'asdasdas', '2012-09-01 11:14:44', '127.0.0.1', 'browser'),
-(6, 'asdas', 'dasda', 'dasdas', 'asdasdad', 'asdasdas', '2012-09-01 11:22:09', '127.0.0.1', 'browser'),
-(7, 'asdas', 'dasda', 'dasdas', 'asdasdad', 'asdasdas', '2012-09-01 11:22:50', '127.0.0.1', 'browser'),
-(8, 'asdas', 'dasda', 'dasdas', 'asdasdad', 'asdasdas', '2012-09-01 11:25:58', '127.0.0.1', 'browser'),
-(9, 'asdas', 'dasda', 'dasdas', 'asdasdad', 'asdasdas', '2012-09-01 11:28:11', '127.0.0.1', 'browser'),
-(10, 'asdas', 'dasda', 'dasdas', 'asdasdad', 'asdasdas', '2012-09-01 11:28:33', '127.0.0.1', 'browser'),
-(11, 'asdas', 'dasda', 'dasdas', 'asdasdad', 'asdasdas', '2012-09-01 11:38:04', '127.0.0.1', 'browser'),
-(12, 'asdasd', 'dasd', 'dasdsa', 'asda', 'adas', '2012-09-01 12:44:19', '127.0.0.1', 'browser'),
-(13, 'sadasd', 'asda', 'asdasd', 'andy', 'neviem', '2012-09-01 12:46:22', '127.0.0.1', 'browser'),
-(14, 'sdfsdfs', 'sdfsdfsdf', 'fsdf', 'sdfs', 'sdfsdf', '2012-09-01 12:47:29', '127.0.0.1', 'browser'),
-(15, 'dfsdfs', 'sfsdfs', 'sdfsdf', 'dfsdf', 'fsdfsd', '2012-09-01 12:47:46', '127.0.0.1', 'browser'),
-(16, 'andy', 'lolofon', 'andy', 'andy', 'staznost', '2012-09-01 16:34:33', '127.0.0.1', 'browser'),
-(18, 'sdf', 'sdfsd', 'sdfsd', 'fsdfsdf', 'dfsd', '2012-09-14 15:54:21', '127.0.0.1', 'browser');
+CREATE TABLE IF NOT EXISTS `tokens` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_u` int(11) NOT NULL,
+  `token` varchar(40) COLLATE utf8_czech_ci NOT NULL,
+  `t_or_f` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nick` varchar(20) COLLATE utf8_czech_ci NOT NULL,
+  `pass` varchar(40) COLLATE utf8_czech_ci NOT NULL,
+  `reg_date` varchar(11) COLLATE utf8_czech_ci NOT NULL,
+  `mail` varchar(40) COLLATE utf8_czech_ci NOT NULL,
+  `last_log` varchar(25) COLLATE utf8_czech_ci NOT NULL,
+  `reg` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
