123400554f38af9b18b53839a2362629a0c2296b
obalco <obal.tom@gmail.com>
2012-09-26 20:55:06 +0200

    ciastocne upravene pre novu databazu...este to neni cele,takze to vyhlasuje chyby

--- index.php
+++ index.php
@@ -100,46 +100,44 @@
             include('functions.php');
       
             echo '<form  method="post">
-                    Co/Kto:<br /><textarea cols="100" rows="1" name="staznost_na"></textarea><br/>
-                    Ako/Cim:<br /><textarea name="staznost" rows="5" cols="100"></textarea><br/><br/>
+                    Co/Kto:<br /><textarea cols="100" rows="1" name="claim_at"></textarea><br/>
+                    Ako/Cim:<br /><textarea name="claim" rows="5" cols="100"></textarea><br/><br/>
                     Nick: <input type="text" name="nick" cols="35"> 
-                    Kedy: <input type="text" name="staznost_kedy" cols="35">
+                    Kedy: <input type="text" name="claim_when" cols="35">
                     E-mail: <input type="text" name="email" cols="35"><br/>
                     <p align="center"><input type="submit" id="button" value="Odosla sažnos" name="send"></p>
                   </form></div>';
                 
             if(isset($_POST['send'] ))
               {
-				$message="";
-                $staznost_na   = mysql_real_escape_string(trim($_POST['staznost_na']));
-                $staznost      = mysql_real_escape_string(trim($_POST['staznost']));
-                $staznost_kedy = mysql_real_escape_string(trim($_POST['staznost_kedy']));
-                //$nick 		   = mysql_real_escape_string(trim($_POST['nick']));
-               // $email 		   = mysql_real_escape_string(trim($_POST['email']));
+                $message="";
+                $claim_at   = mysql_real_escape_string(trim($_POST['staznost_na']));
+                $claim      = mysql_real_escape_string(trim($_POST['staznost']));
+                $claim_when = mysql_real_escape_string(trim($_POST['staznost_kedy']));
+                $nick 		   = mysql_real_escape_string(trim($_POST['nick']));
+                $email 		   = mysql_real_escape_string(trim($_POST['email']));
                 $ip			   = getIpAddress();
+				
 				
 				// slovo ln spojene s premennou bude oznacovat dlzku retazca premennej
 				// slovo ok budeme spajat s premenou a bude typu boolean
 				$ln_staznost_na = strlen($staznost_na);
 				$ln_staznost    = strlen($staznost);
 
-				if($ln_staznost_na <3 && $ln_staznost_na > 20) {$ok_staznost_na = true; } else {$ok_staznost_na = false;$message.=$error[6];}
+				if($ln_staznost_na <3 && $ln_staznost_na > 50) {$ok_staznost_na = true; } else {$ok_staznost_na = false;$message.=$error[6];}
 				if($ln_staznost <5 && $ln_staznost > 200) {$ok_staznost = true; } 		  else {$ok_staznost = false; $message.=$error[7];}
+			
 				// blo by fajn keby nick a mail sa dava do session :) teda pri logovaní :)
 				
             
-                if ($ok_staznost_na === true && $ok_staznost === true)
+            if ($ok_staznost_na === true && $ok_staznost === true)
                   {
                     include('db.php');
-                    $sql  = "INSERT INTO staznosti (staznost_na, staznost, staznost_kedy, nick, email, datum_staznost , ip  , browser) 
-                             VALUES ('$staznost_na','$staznost','$staznost_kedy', '$nick', '$email', NOW(), '$ip', 'browser')";
-					$res  = mysql_query($sql);
-					$id_s = mysql_insert_id(); // funkcia mysql_insert_id dostava poslednu autoinkrementovanu hodnotu primarneho kluca u nas to je id 
-					
-					$sql  = $vys = ""; // pre istotu vynulovanie premennych
-                    $sql  = "INSERT INTO comments (id_staznosti, comment) 
-                             VALUES ('$id_s','komentar')";
-					$vys  = mysql_query($sql);
+                    $sql  = "INSERT INTO claims (id_u, who, how, date, ,ip  ,sys_date) 
+                             VALUES ('$id_u','$claim_at','$claim', '$claim_when', '$ip', NOW())";
+                    $res  = mysql_query($sql);
+                    $id_s = mysql_insert_id(); // funkcia mysql_insert_id dostava poslednu autoinkrementovanu hodnotu primarneho kluca u nas to je id 
+                    $vys  = mysql_query($sql);
                     header("Location:index.php");
                   }
                 else
@@ -158,7 +156,7 @@
         <?php
           include('db.php');
                   
-          $sql="SELECT * FROM staznosti order BY id DESC LIMIT 10 ";
+          $sql="SELECT * FROM claims order BY id DESC LIMIT 10 ";
           $res=mysql_query($sql);
           $pocet=mysql_num_rows($res);
           
@@ -182,13 +180,13 @@
            }
          echo'</div>';
          
-          $sql="SELECT * FROM staznosti ";
+         $sql="SELECT * FROM claims ";
          $res=mysql_query($sql);
          $pocet=mysql_num_rows($res);
          
          if($pocet>10) 
             {
-             echo '<p><a href="vypis.php">?alej</a></p>';
+             echo '<p><a href="vypis.php">Ïalej</a></p>';
             }
 		?>
       <p align="center" class="pata">Code and Design by <a href="www.am.6f.sk" target="_blank"><img src="images/am_logo.png"  height="15" alt="AM PAGE Andrej Majik Logo"></a>
